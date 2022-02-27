d170c25a4f4d7fe0a8a3b9a75d9f26ea5367e2ab
sean <scorbett@thezebra.com>
2022-02-27 15:02:12 -0600

    removed zebra deps

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(current_directory, 'README.md'), "r") as readme:
 
 setup(
     name="hedra",
-    version="0.1.2",
+    version="0.1.3",
     description="Powerful performance testing made easy.",
     long_description=package_description,
     long_description_content_type="text/markdown",
@@ -48,6 +48,7 @@ setup(
         'asyncio-pool',
         'playwright',
         'py3cli-tools',
+        'py3-async-tools',
         'easy-logger-py',
         'broadkast',
         'statstream-py',
