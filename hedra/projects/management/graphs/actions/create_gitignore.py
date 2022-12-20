5f358272470ade43d121e6fd8f5964dbeb60e9d2
scorbettUM <sean.corbett@umontana.edu>
2022-12-20 00:45:05 -0600

    sc-dev: added --project-name arg to hedra project create and added all reporter types for generated stages

--- hedra/projects/management/graphs/actions/create_gitignore.py
+++ hedra/projects/management/graphs/actions/create_gitignore.py
@@ -14,10 +14,14 @@ class CreateGitignore(RepoAction):
         ).__init__(config, graph_files)
 
     def execute(self):
+
+        self.logger.hedra.sync.debug(f'CreateGitignore: {self.action_id} - creating .gitignore at path - {self.config.path}')
         self._setup()
         self._update_ignore(force_create=True)
 
         current_time = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
         gitignore_mmessage = f"Hedra graph update: {self.config.path} - {current_time}" 
 
+
+        self.logger.hedra.sync.debug(f'CreateGitignore: {self.action_id} - making local commit to add new .gitignore at path - {self.config.path}')
         self.repo.index.commit(gitignore_mmessage)
