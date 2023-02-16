705bb54268cb1d6e43f53eaa7073ff423df5a34b
Sean Corbett <sean.corbett@umontana.edu>
2023-02-15 22:01:36 -0600

    Update Setup stage edge

--- hedra/core/graphs/transitions/setup/setup_edge.py
+++ hedra/core/graphs/transitions/setup/setup_edge.py
@@ -118,8 +118,17 @@ class SetupEdge(BaseEdge[Setup]):
 
     def _update(self, destination: Stage):
 
-        history = self.history[(self.from_stage_name, self.source.name)]
+        for edge_name in self.history:
 
+            history = self.history[edge_name]
+
+            self.next_history[edge_name] = {}
+
+            self.next_history[edge_name].update({
+                key: value for key, value  in history.items() if key in self.provides
+            })
+
+        history = self.history[(self.from_stage_name, self.source.name)]
 
         ready_stages = history.get('setup_stage_ready_stages', {})
         setup_candidates = history.get('setup_stage_candidates', {})
@@ -129,85 +138,20 @@ class SetupEdge(BaseEdge[Setup]):
 
         if setup_execute_stage:
             execute_stage_setup_hooks = setup_execute_stage.context['execute_stage_setup_hooks']
-        
 
         self.stages_by_type[StageTypes.EXECUTE].update(ready_stages)
 
-        self.next_history[(self.source.name, destination.name)] = {
+        self.next_history[(self.source.name, destination.name)].update({
             'execute_stage_setup_hooks': execute_stage_setup_hooks,
             'setup_stage_ready_stages': ready_stages,
             'setup_stage_candidates': list(setup_candidates.keys()),
             'execute_stage_setup_config': setup_config,
             'execute_stage_setup_by': self.source.name   
-        }
+        })
         
 
     def split(self, edges: List[BaseEdge]) -> None:
-        edges_count = len(edges)
-        setup_candidates = self.get_setup_candidates()
-        candidate_names = list(setup_candidates.keys())
-        candidates_count = len(candidate_names)
-
-        candidates_per_edge = math.floor(candidates_count/edges_count)
-
-        if self.transition_idx == (edges_count - 1):
-            candidates_per_edge += candidates_count%edges_count
-
-        batch_start = candidates_per_edge * self.transition_idx
-        batch_end = batch_start + candidates_per_edge + 1
-
-        selected_candidates = candidate_names[batch_start:batch_end]
-        self.assigned_candidates = selected_candidates
-
-        discovered: Dict[str, Stage] = import_stages(self.source.graph_path)
-        
-        initialized_stages = {}
-        hooks_by_type = defaultdict(dict)
-        hooks_by_name = {}
-        hooks_by_shortname = defaultdict(dict)
-
-        generated_hooks = {}
-        for stage in discovered.values():
-            stage: Stage = stage()
-            stage.graph_name = self.source.graph_name
-            stage.graph_path = self.source.graph_path
-            stage.graph_id = self.source.generation_id
-
-            for hook_shortname, hook in registrar.reserved[stage.name].items():
-                hook._call = hook._call.__get__(stage, stage.__class__)
-                setattr(stage, hook_shortname, hook._call)
-
-            initialized_stage = set_stage_hooks(
-                stage, 
-                generated_hooks
-            )
-
-            for hook_type in initialized_stage.hooks:
-
-                for hook in initialized_stage.hooks[hook_type]:
-                    hooks_by_type[hook_type][hook.name] = hook
-                    hooks_by_name[hook.name] = hook
-                    hooks_by_shortname[hook_type][hook.shortname] = hook
-
-            initialized_stages[initialized_stage.name] = initialized_stage
-
-        setup_stage: Setup = initialized_stages.get(self.source.name)
-        setup_stage.context.update(self.source.context)
-        setup_stage.plugins = self.source.plugins
-
-        for hook_type in setup_stage.hooks:
-            for hook in setup_stage.hooks[hook_type]:
-                hooks_by_type[hook_type][hook.name] = hook
-
-        events_graph = EventGraph(hooks_by_type)
-        events_graph.hooks_by_name = hooks_by_name
-        events_graph.hooks_by_shortname = hooks_by_shortname
-        events_graph.hooks_to_events().assemble_graph().apply_graph_to_events()
-
-        for stage in initialized_stages.values():
-            stage.dispatcher.assemble_action_and_task_subgraphs()
-
-        self.source = setup_stage
+        pass
 
     def get_setup_candidates(self) -> Dict[str, Execute]:
         execute_stages = [(stage_name, stage) for stage_name, stage in self.stages_by_type.get(StageTypes.EXECUTE).items()]
