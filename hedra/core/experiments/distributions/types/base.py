7cd05e7f4e230ca522414601cb296728f2215130
Ada Lundhe <ada@datavant.com>
2023-04-19 16:43:59 -0500

    sc-dev: added distribution calculation

--- hedra/core/experiments/distributions/types/base.py
+++ hedra/core/experiments/distributions/types/base.py
@@ -1,4 +1,6 @@
-from typing import Union
+import math
+import numpy
+from typing import Union, List, Tuple
 from numpy.random import normal
 
 class SciPyDistribution:
@@ -21,11 +23,93 @@ class BaseDistribution:
         self.randomness = randomness
         self._frozen_distribution = frozen_distribution
         self._noise_scale = 0.01
+        self._lower_bound = 0.1
+        self._upper_bound = 0.9
 
-    def generate(self):
-        return self._frozen_distribution.rvs(
-            self.size
+    def generate_distribution(self, batch_size: int) -> List[float]:
+        distribution_size, step_size = self.get_distribution_and_step_size()
+
+        distribution_sample = self._frozen_distribution.rvs(
+            distribution_size
         ) * normal(
             scale=self._noise_scale,
-            size=self.size
+            size=distribution_size
         )
+
+        generated_walk = self.generate_random_walk(distribution_sample)
+        scaled_walk = self.smooth_generated_walk(
+            generated_walk,
+            step_size
+        ) * batch_size
+
+        return self.apply_averaging(
+            scaled_walk,
+            step_size
+        )
+        
+    
+    def get_distribution_and_step_size(self) -> Tuple[int, int]:
+        if self.size <= 10:
+            distribution_size = self.size**3
+            step_size = self.size**2
+
+        elif self.size < 100:
+            distribution_size = self.size**2
+            step_size = self.size
+
+        elif self.size < 1000:
+            distribution_size = self.size * int(math.sqrt(self.size))
+            step_size = int(math.sqrt(self.size))
+            
+        else:
+            distribution_size = self.size * (1 + int(
+                math.sqrt(self.size)/self.size
+            ))
+
+            step_size = (1 + int(
+                math.sqrt(self.size)/self.size
+            ))
+
+        return distribution_size, step_size
+    
+    def generate_random_walk(self, distribution_sample: List[float]) -> List[float]:
+        current_step_value = 0.5
+        result = []
+        for sample_value in distribution_sample:
+
+            if current_step_value < self._lower_bound:
+                current_step_value = self._lower_bound
+
+            elif current_step_value > self._upper_bound:
+                current_step_value = self._upper_bound
+
+            result.append(current_step_value)
+            current_step_value += sample_value
+
+        return numpy.array(result)
+    
+    def smooth_generated_walk(
+        self, 
+        generated_walk: List[float], 
+        smoothing_window_size: int
+    ) -> List[float]:
+        return numpy.convolve(
+            generated_walk, 
+            numpy.ones(
+                (smoothing_window_size,)
+            )/smoothing_window_size
+        )[(smoothing_window_size-1):]
+    
+    def apply_averaging(
+        self, 
+        scaled_walk: List[float],
+        step_size: int
+    ) -> List[float]:
+        averaged_data = []
+
+        for idx in range(0, len(scaled_walk), step_size):
+            averaged_data.append(
+                numpy.mean(scaled_walk[idx:idx + self.size])
+            )
+
+        return averaged_data
